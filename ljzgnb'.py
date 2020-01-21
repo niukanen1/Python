c0eb32db19660477c1e9354417590560201db0a8
niukanen1 <57656076+niukanen1@users.noreply.github.com>
2020-01-21 13:34:18 +0200

    Update ljzgnb'.py

--- ljzgnb'.py
+++ ljzgnb'.py
@@ -106,35 +106,99 @@ m.decreasehp()
 
 ###########warrior
 import time
+''' Creating main class'''
 class warrior():
    hp1=hp2 = 100   
    dam = 20
    def cl(self):        
         a = 0
-        while self.hp1 or self.hp2 > 0:
-           r = random.randint(1,2)          
-           time.sleep(1)
-           if r == 1:
-               self.hp1 -= self.dam
-               print('Player 2 hits PLayer 1')
-           else:
-               self.hp2 -= self.dam
-               print('Player 1 hits PLayer 2')
-           a += 1
-           time.sleep(1)
-           print('Player 1 loses {0} HP \nalready lost {1} HP \n {2} HP left \n {3}'.format(self.dam, ((self.dam) * a), (100 - ((self.dam) * a)), ('-')*10))
-        if self.hp1 == 0:
-            print('Player 1 is dead! \n Player 2 wins')
-        else:
-            print('Player 2 is dead! \n Player 1 wins')
-        if self.hp1 or self.hp2 == 0:
+        while self.hp1 and self.hp2 > 0:
+            '''while to count getting damage'''
+            r = random.randint(1,2)          
+            time.sleep(1)
+            a += 1
+            if r == 1:
+                self.hp1 -= self.dam
+                print('Player 2 hits PLayer 1')
+                print('Player 1 loses {0} HP \nalready lost {1} HP \n {2} HP left \n {3}'.format(self.dam, ((self.dam) * a), (100 - ((self.dam) * a)), ('-')*10))
+            else:
+                self.hp2 -= self.dam
+                print('Player 1 hits PLayer 2')
+                print('Player 2 loses {0} HP \nalready lost {1} HP \n {2} HP left \n {3}'.format(self.dam, ((self.dam) * a), (100 - ((self.dam) * a)), ('-')*10))
+            
+            time.sleep(1)
+            '''ending '''
+            if self.hp1 == 0:
+                print('Player 1 is dead! \n Player 2 wins')
+            elif self.hp2 == 0:
+                print('Player 2 is dead! \n Player 1 wins')
+          
 
             
            
 w = warrior()
-w.cl()               
-               
-               
+w.cl()
+
+
+
+
+####################game
+
+import random
+''' Create class and call it one to use it as the parent class '''
+class one:
+    '''generator with every soldier number and soldier's command'''
+    def __init__(self, num, commandid):
+        self.num = num
+        self.commandId = commandid
+        ''' Another generator'''
+class hero(one): 
+    def __init__(self,  num, commandid, name, lvl=1):
+        one.__init__(self, num, commandid)
+        self.name = name
+        self.lvl = lvl
+
+    def getlevel(self):
+        return self.lvl
+
+
+
+    def inclvl(self): 
+        
+        ''' Increase lvl if command contains more soldiers'''
+        
+        self.lvl += 1
+        print('Уровень героя', self.name,'увеличен на 1 и равен', self.lvl)
+        
+class sol(one):
+    ''' class and functiion to make soldier follow hero1'''
+    def gotohero(self, Hero):
+        print('Солдат ', self.num, 'следует за героем', Hero.name, 'с номером', Hero.num)
+H1 = hero(1, 1, '1')
+H2 = hero(2, 2, '2')
+armyH1, armyH2 = [], []
+
+for i in range(3, 10):
+    '''Adding soldiers to teams randomly '''
+    n = random.randint(0, 1)
+    if n:
+        armyH1.append(sol(i, 1))
+        print('Солдат с номером', i, 'добавлен в армию', H1.name)
+    else:
+        armyH2.append(sol(i, 2))
+        print('Солдат с номером', i, 'добавлен в армию', H2.name)
+
+print('Армия героя', H1.name, ' - ', len(armyH1))
+print('Армия героя', H2.name, ' - ', len(armyH2))
+
+if len(armyH1) > len(armyH2):
+    print('В армии', H1.name, 'больше солдат - увеличиваем его уровень')
+    '''Condition to increase hero lvl'''
+    H1.inclvl()
+else:
+    print('В армии', H2.name, 'больше солдат - увеличиваем его уровень')
+    H2.inclvl()
+armyH1[1].gotohero(H2)        
         
     
 
